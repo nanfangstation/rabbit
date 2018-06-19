2bf4b91c0627f0fb2795f33887f2d37a03919397
pengnf <pengnf@guahao.com>
2018-06-20 00:14:24 +0800

    20180620_去掉copyright

--- helper/src/main/java/GenerateDefaultInstance.java
+++ helper/src/main/java/GenerateDefaultInstance.java
@@ -1,11 +1,3 @@
-/*
- * Copyright (c) 2001-2018 GuaHao.com Corporation Limited. All rights reserved.
- * This software is the confidential and proprietary information of GuaHao Company.
- * ("Confidential Information").
- * You shall not disclose such Confidential Information and shall use it only
- * in accordance with the terms of the license agreement you entered into with GuaHao.com.
- */
-
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
